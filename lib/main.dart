82587580df5689779d76423ca39727468e1bb8d4
Sanzoo <saintsan196@gmail.com>
2024-12-10 15:29:28 +0700

    implement native splash screen and update pubspec.lock dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:slicing_ui/pages/login_page.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    print('pause 2 seconds');
+    await Future.delayed(const Duration(seconds: 1));
+    print('unpause');
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
